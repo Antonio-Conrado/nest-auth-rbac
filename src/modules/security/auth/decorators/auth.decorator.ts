cb215eaf219711c586127f4f60fabc18f02acfb7
Antonio-Conrado <jconradolopez19@gmail.com>
2025-09-08 18:24:01 -0600

    Added Swagger Bearer token support in Auth decorator

--- src/modules/security/auth/decorators/auth.decorator.ts
+++ src/modules/security/auth/decorators/auth.decorator.ts
@@ -2,9 +2,11 @@ import { applyDecorators, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { PermissionProtected } from './permission-protected.decorator';
 import { UserPermissionGuard } from '../guards/user-permission.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 export function Auth(...permissions: string[]) {
   return applyDecorators(
+    ApiBearerAuth(), // Configures Swagger to send the Bearer token in the Authorization header when testing the API
     PermissionProtected(...permissions),
     UseGuards(AuthGuard('jwt'), UserPermissionGuard),
   );
