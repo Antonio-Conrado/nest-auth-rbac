e413ff03f6b7c945c7846885b6cb937f7916c5ac
Antonio-Conrado <jconradolopez19@gmail.com>
2025-09-08 18:16:30 -0600

    Refactored Joi schema: added cache, cors, throttle, jwt expires and improved ordering

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,12 +1,16 @@
 import * as Joi from 'joi';
 
 export const JoiValidationSchema = Joi.object({
+  // APP
   NODE_ENV: Joi.string().required().messages({
     'any.required': 'The environment variable NODE_ENV is required',
   }),
   BACKEND_URL: Joi.string().required().messages({
     'any.required': 'The environment variable BACKEND_URL is required',
   }),
+  PORT: Joi.number().default(3000),
+
+  // DATABASE
   DATABASE_HOST: Joi.string().required().messages({
     'any.required': 'The environment variable DATABASE_HOST is required',
   }),
@@ -22,9 +26,45 @@ export const JoiValidationSchema = Joi.object({
   DATABASE_NAME: Joi.string().required().messages({
     'any.required': 'The environment variable DATABASE_NAME is required',
   }),
+
+  // AUTH
   JWT_SECRET: Joi.string().required().messages({
     'any.required': 'The environment variable JWT_SECRET is required',
   }),
-  PORT: Joi.number().default(3000),
+
+  // CONFIGURATION
+  // CORS
+  ORIGIN: Joi.string().required().messages({
+    'any.required': 'The environment variable CORS is required',
+  }),
+
+  // Rate limiting configuration
+  THROTTLE_TTL: Joi.number().default(60000).messages({
+    'number.base': 'The environment variable THROTTLE_TTL must be a number',
+  }),
+  THROTTLE_LIMIT: Joi.number().default(100).messages({
+    'number.base': 'The environment variable THROTTLE_LIMIT must be a number',
+  }),
+
+  // Cache configuration
+  CACHE_TTL: Joi.number().default(10).messages({
+    'number.base': 'The environment variable CACHE_TTL must be a number',
+  }),
+  CACHE_MAX: Joi.number().default(100).messages({
+    'number.base': 'The environment variable CACHE_MAX must be a number',
+  }),
+  CACHE_STORE: Joi.string().default('memory').messages({
+    'string.base': 'The environment variable CACHE_STORE must be a string',
+  }),
+  // JWT configuration
+  JWT_EXPIRES_IN: Joi.alternatives()
+    .try(Joi.number(), Joi.string())
+    .default('1h')
+    .messages({
+      'alternatives.types':
+        'The environment variable JWT_EXPIRES_IN must be a number (seconds) or a string',
+    }),
+
+  // Maximum attempts to request the .env variables
   DEFAULT_LIMIT: Joi.number().default(5),
 });
