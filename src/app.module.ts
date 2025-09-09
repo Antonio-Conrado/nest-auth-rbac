226a6e91324e2da50524073e386b3dbb4062a2a9
Antonio-Conrado <jconradolopez19@gmail.com>
2025-09-08 18:29:57 -0600

    Added Swagger, CORS, Helmet, cache and global throttling configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,10 @@ import { RolesModule } from './modules/security/roles/roles.module';
 import { PermissionsModule } from './modules/security/permissions/permissions.module';
 import { UsersModule } from './modules/security/users/users.module';
 import { SeedModule } from './seed/seed.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { CacheModule } from '@nestjs/cache-manager';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerConfigService } from './config/throttler.config';
 
 @Module({
   imports: [
@@ -17,12 +21,29 @@ import { SeedModule } from './seed/seed.module';
       isGlobal: true,
       validationSchema: JoiValidationSchema,
     }),
+
+    // Rate limiting configuration for requests
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: ThrottlerConfigService,
+    }),
+    // cache
+    CacheModule.registerAsync({
+      inject: [ConfigService],
+      isGlobal: true,
+      useFactory: (config: ConfigService) => ({
+        store: config.get<string>('CACHE_STORE', 'memory'),
+        ttl: config.get<number>('CACHE_TTL'),
+        max: config.get<number>('CACHE_MAX'),
+      }),
+    }),
+
+    // Initialize TypeORM using the config service
     TypeOrmModule.forRootAsync({
-      // Initialize TypeORM using the config service
       useFactory: typeOrmConfig,
       inject: [ConfigService],
     }),
-
+    //modules
     AuthModule,
     RolesModule,
     PermissionsModule,
@@ -30,6 +51,6 @@ import { SeedModule } from './seed/seed.module';
     SeedModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AuthExceptionFilter } from './common/exceptions/auth-exception-filter';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('api');
+  app.use(helmet());
   app.useGlobalFilters(new AuthExceptionFilter());
 
   //enables global validation for incoming data
@@ -18,13 +20,36 @@ async function bootstrap() {
     }),
   );
 
+  //cors
+  app.enableCors({
+    origin: process.env.ORIGIN,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+  });
+
+  // Enable API versioning
+  // Use @Version('1'), @Version('2'), etc. in controllers and endpoints
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  // Enable graceful shutdown
+  app.enableShutdownHooks();
+
   const config = new DocumentBuilder()
     .setTitle('Nest Auth RBAC')
     .setDescription('Nest Auth RBAC')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/v1', app, documentFactory);
+
+  const document = SwaggerModule.createDocument(app, config, {
+    operationIdFactory: (controllerKey: string, methodKey: string) => {
+      // Delete the "Controller" suffix from the controller name
+      return `${controllerKey.replace('Controller', '')}_${methodKey}`;
+    },
+  });
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
